b7265255dd9ea7e46ea585fdacd6ffc7611d649a
Elto Borges <eltobrgz.code@gmail.com>
2025-06-30 11:00:10 -0300

    atualizaçao2

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Permite requisições de qualquer origem
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
